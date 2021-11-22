a9526e2db1477270eeaea16a16ab671a7a56cae9
Theee Sheeep <79360988+thatonesheep80@users.noreply.github.com>
2021-11-22 16:34:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+video = "";
+input_var = "";
+
+function preload()
+{
+    video = createVideo();
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+   objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+   document.getElementById("status").innerHTML = "Status : Detecting Objects";
+   input_var = document.getElementById("input").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
