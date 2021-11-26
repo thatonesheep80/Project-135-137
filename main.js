0a3dd9b89466d0ab215c58c3db0d88a870b05395
Theee Sheeep <79360988+thatonesheep80@users.noreply.github.com>
2021-11-26 17:11:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 video = "";
 input_var = "";
+objects = [];
+status = "";
 
 function preload()
 {
@@ -29,7 +31,40 @@ function modelLoaded()
 {
     console.log("Model Loaded!");
     status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+     if(status != "")
+    {
+    image(video, 0, 0, 480, 380);
+    objectDetector.detect(gotResult)
+
+        for(i = 0; i < objects.length; i++) 
+        {
+            if(objects[i].label == input_var)
+            {
+            document.getElementById("status").innerHTML = input_var + " Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects detected are - "+ objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height)
+            }
+        }
+    }
 }
